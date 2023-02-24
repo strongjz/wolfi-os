ae6c1911003bffef31dc2739444d639981f5d36b
James Strong <james.strong@chainguard.dev>
2023-02-24 13:29:22 -0500

    testing moving languages
    Signed-off-by: James Strong <james.strong@chainguard.dev>

--- lint.sh
+++ lint.sh
@@ -2,16 +2,22 @@
 
 set -euo pipefail
 
-for f in *.yaml; do
-  echo "---" $f
+array=()
+while IFS=  read -r -d $'\0'; do
+    array+=("$REPLY")
+done < <(find . -name "*.yaml" -not -path "./.github/*" -print0)
+
+for f in "${array[@]}"; do
+  echo "---" "${f}"
 
   # Check that every name-version-epoch is defined in Makefile
-  name=$(yq '.package.name' $f)
-  version=$(yq '.package.version' $f)
-  epoch=$(yq '.package.epoch' $f)
+  name=$(yq '.package.name' "${f}")
+  version=$(yq '.package.version' "${f}")
+  epoch=$(yq '.package.epoch' "${f}")
   want="build-package,${name},${version}-r${epoch}"
-  if ! grep -q $want Makefile; then
-    echo "missing $want in Makefile"
+
+  if ! grep -q "${want}" Makefile; then
+    echo "missing ${want} in Makefile"
     exit 1
   fi
 
@@ -19,9 +25,9 @@ for f in *.yaml; do
   # Packages from the bootstrap repo should be allowed, but otherwise packages
   # should be fetched locally and the local repository should be appended at
   # build time.
-  if grep -q packages.wolfi.dev/os $f; then
-    yq -i 'del(.environment.contents.repositories)' $f
-    yq -i 'del(.environment.contents.keyring)' $f
+  if grep -q packages.wolfi.dev/os "${f}"; then
+    yq -i 'del(.environment.contents.repositories)' "${f}"
+    yq -i 'del(.environment.contents.keyring)' "${f}"
   fi
 done
 
